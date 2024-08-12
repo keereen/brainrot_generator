ab5b9cfe63211f926c31d7ecb75f493a06a9f931
keeran <66531762+keereen@users.noreply.github.com>
2024-08-12 07:14:45 +0100

    basic reddit api usage

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -11,5 +11,16 @@ reddit = praw.Reddit(
 
 print(reddit)
 
-def top_posts_of_day():
-    ''
+def top_posts_from_subreddit(reddit, time_filter_value, post_limit, subreddit_name):
+    # time_filter specifies 'top' 'new' 'hot' etc
+    # post_limit specifies how many posts to scrape
+    # subreddit_name is the subreddit to scrape
+
+    subreddit = reddit.subreddit(subreddit_name)
+    subreddit_top = subreddit.top(time_filter = time_filter_value, limit = post_limit)
+    print(subreddit_top)
+    for post in subreddit_top:
+        print(post.selftext)
+    # remove stickied posts as an optional toggle
+
+top_posts_from_subreddit(reddit, "day", 10, "AITAH")
